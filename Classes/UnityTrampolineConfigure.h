9fa86f4cfa2bd6b551fd3b268132ad69175b3a8a
Ontario Britton <ontario.britton@gmail.com>
2014-04-21 11:46:43 -0600

    DONE: Nanos not matching (hairstyles and shape) DONE: Nanos traversing grid and then matching DONE: Nanos flying into network at random DONE: Nanos not going into network DONE all nanos poppable on network touch.

--- Classes/UnityTrampolineConfigure.h
+++ Classes/UnityTrampolineConfigure.h
@@ -6,7 +6,7 @@
 //   UNITY_VERSION value will be changed
 //   UNITY_X_Y_Z for this new version will be added
 
-#define UNITY_VERSION 433
+#define UNITY_VERSION 434
 
 // known unity versions
 #define UNITY_4_2_0 420
@@ -14,4 +14,4 @@
 #define UNITY_4_2_2 422
 #define UNITY_4_3_0 430
 #define UNITY_4_3_1 431
-#define UNITY_4_3_3 433
+#define UNITY_4_3_4 434
